66cc4d4ef39c79ec312b7a71fd574d1b119db26c
Henrik Hårshagen <henha806@student.liu.se>
2024-05-07 11:41:28 +0200

    Added solution to labN

--- lab/labn/pn-skeleton.py
+++ lab/labn/pn-skeleton.py
@@ -2,10 +2,10 @@
 # Numeriska metoder
 # Skelettkod
 
-# Er uppgift ‰r att implementera Newtons metod fˆr att approximera
-# nollst‰llen till en funktion och att sedan till‰mpa denna
-# implementation fˆr att lˆsa tre numeriska ber‰kningsproblem. En
-# beskrivning av metoden hittar du h‰r:
+# Er uppgift √§r att implementera Newtons metod f√∂r att approximera
+# nollst√§llen till en funktion och att sedan till√§mpa denna
+# implementation f√∂r att l√∂sa tre numeriska ber√§kningsproblem. En
+# beskrivning av metoden hittar du h√§r:
 #
 # https://sv.wikipedia.org/wiki/Newtons_metod
 
@@ -29,13 +29,13 @@ def newton_one(f, f_prime, x0):
         The next Newton approximation to a root of the function `f`, based on
         the initial guess `x0`.
     """
-    # TODO: Replace the next line with your own code.
-    return 0.0
+    return x0-(f(x0)/f_prime(x0))
+
 
 
 # ### Problem 2
 #
-# Implementera en fullst‰ndig Newton-approximation.
+# Implementera en fullst√§ndig Newton-approximation.
 
 
 def newton(f, f_prime, x0, n_digits=6):
@@ -56,60 +56,64 @@ def newton(f, f_prime, x0, n_digits=6):
         iterative process should end when the approximation does not alter the
         first `n_digits` after the decimal place.
     """
-    # TODO: Replace the next line with your own code.
-    return 0.0, 0
 
+    current_approx = x0
+    iterations_needed = 0
+
+    while True:
+        iterations_needed += 1
+        old_approx = current_approx
+        current_approx = newton_one(f, f_prime, current_approx)
+        if round(current_approx,n_digits) == round(old_approx,n_digits):
+            return (current_approx, iterations_needed)
 
 # ## Del 2
 
 # ### Problem 3
 #
-# Anv‰nd din kod fˆr att approximera nollst‰llet av funktionen
+# Anv√§nd din kod f√∂r att approximera nollst√§llet av funktionen
 #
-# f(x) = x^3 - x + 1
+# f(x) = x^3 - x + 1 =? 0
 #
-# Ange Python-funktioner `f` och `f_prime` och ett l‰mpligt startv‰rde `x0`
+# Ange Python-funktioner `f` och `f_prime` och ett l√§mpligt startv√§rde `x0`
 # och skriv ut ert numeriska resultat inklusive antalet iterationer.
 # Noggrannheten ska vara sex siffror efter kommat.
 
 
 def problem3():
-    # TODO: Replace the following lines with your own code.
-    f = lambda x: 0.0
-    f_prime = lambda x: 1.0
-    x0 = 0.0
+    f = lambda x: pow(x,3) - x + 1 
+    f_prime = lambda x: 3* pow(x,2) - 1 
+    x0 = -1.5
     a, i = newton(f, f_prime, x0)
     print("approximation: {:.6f}, number of iterations: {}".format(a, i))
 
 
 # ### Problem 4
-#
-# Anv‰nd er kod fˆr att approximativt ber‰kna den femte roten ur 5. Bˆrja med
-# att ange den funktion vars nollst‰lle ni vill approximera. Forts‰tt sedan
-# som i fˆregÂende uppgift.
+# f(x) = x‚Åµ - 5 => 0 = x‚Åµ - 5 => x‚Åµ = 5
+# Anv√§nd er kod f√∂r att approximativt ber√§kna den femte roten ur 5. B√∂rja med
+# att ange den funktion vars nollst√§lle ni vill approximera. Forts√§tt sedan
+# som i f√∂reg√•ende uppgift.
 
 
 def problem4():
-    # TODO: Replace the following lines with your own code.
-    f = lambda x: 0.0
-    f_prime = lambda x: 1.0
-    x0 = 0.0
+    f = lambda x: pow(x, 5) - 5
+    f_prime = lambda x: 5 * pow(x, 4)
+    x0 = 1.5
     a, i = newton(f, f_prime, x0)
     print("approximation: {:.6f}, number of iterations: {}".format(a, i))
 
 
 # ### Problem 5
 #
-# Ange en funktion och tvÂ olika startv‰rden sÂdana att Newtons metod r‰knar
-# ut tvÂ helt olika approximationer.
+# Ange en funktion och tv√• olika startv√§rden s√•dana att Newtons metod r√§knar
+# ut tv√• helt olika approximationer.
 
 
 def problem5():
-    # TODO: Replace the following lines with your own code.
-    f = lambda x: 0.0
-    f_prime = lambda x: 1.0
-    x01 = 0.0
-    x02 = 0.0
+    f = lambda x: pow(x, 2) -1
+    f_prime = lambda x: 2*x
+    x01 = -2
+    x02 = 2
     a1, _ = newton(f, f_prime, x01)
     a2, _ = newton(f, f_prime, x02)
     print("approximation 1: {:.6f}".format(a1))
